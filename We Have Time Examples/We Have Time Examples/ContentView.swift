1a5129051b7e13d80e6b60d9f6bcd6458fe3e833
Natalia Terlecka <natalia@wehavesun.com>
2024-11-18 12:45:46 +0000

    Initial project setup.

---
+++ We Have Time Examples/We Have Time Examples/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  We Have Time Examples
+//
+//  Created by Natalia Terlecka on 18/11/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ We Have Time Examples/We Have Time Examples/We_Have_Time_ExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  We_Have_Time_ExamplesApp.swift
+//  We Have Time Examples
+//
+//  Created by Natalia Terlecka on 18/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct We_Have_Time_ExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
